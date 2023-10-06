7b2a1538c890bd1957e04980ea7a727a5cc60e95
Victor Leonel <111584457+v-leonel@users.noreply.github.com>
2023-10-05 21:56:33 -0300

    Adicionando código do arduino atualizado com delay.

--- codigoArduino/codigoArduino.ino
+++ codigoArduino/codigoArduino.ino
@@ -9,9 +9,9 @@
 
 #include "DHT.h"
 
-#define DHTPIN A5
-#define LM35PIN A0
-#define LUMIPIN A1
+#define DHTPIN A1
+#define LM35PIN A5
+#define LUMIPIN A0
 #define CHAVPIN 7
 
 
@@ -27,19 +27,19 @@ void loop() {
   float dht11_umidade = dht.readHumidity();
   float dht11_temperatura = dht.readTemperature();
   Serial.print(dht11_umidade);
-  Serial.print(";");
+  Serial.print(",");
   Serial.print(dht11_temperatura);
-  Serial.print(";");
+  Serial.print(",");
 
   float luminosidade = analogRead(LUMIPIN);
   Serial.print(luminosidade);
-  Serial.print(";");
+  Serial.print(",");
 
   float lm35_temperatura = analogRead(LM35PIN);
   lm35_temperatura = lm35_temperatura * 0.00488;
   lm35_temperatura = lm35_temperatura * 100;
   Serial.print(lm35_temperatura);
-  Serial.print(";");
+  Serial.print(",");
   
   int chave = digitalRead(7);
   if (chave == 0) {
@@ -48,4 +48,5 @@ void loop() {
     Serial.print("0");
   }
   Serial.println();
+  delay(2000);
 }
